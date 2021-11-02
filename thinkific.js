e8024d65a52ef36078258c1177b16dfb312c37df
Alex <alextheprogrammer21@gmail.com>
2021-11-02 16:28:44 -0700

    added up to the ability to sort and export a csv file

--- thinkific.js
+++ thinkific.js
@@ -1,17 +1,17 @@
 // const args = process.argv.slice(2);
-
-// console.log(args);
-// Use process argv to get the file path. Make it if they put the whole C:// then 
-// put the whole file path, otherwise just assume it's in the same folder
-
-
 const fs = require('fs');
 const Papa = require('papaparse');
 
 const csvFilePath = 'tht-coop.csv'
-
-// Function to read csv which returns a promise so you can do async / await.
-
+//Helper functions
+function uniformString(str) {
+ str = str.split(' ').join('').toLowerCase().substring(0,16);
+ return str;
+}
+function compare(a,b) {
+ return (a.Vendor < b.Vendor ? -1 : 1)
+}
+//Reads csv data passed in through command line
 const readCSV = async (filePath) => {
   const csvFile = fs.readFileSync(filePath)
   const csvData = csvFile.toString()  
@@ -19,16 +19,46 @@ const readCSV = async (filePath) => {
     Papa.parse(csvData, {
       header: true,
       complete: results => {
-        console.log('Complete', results.data.length, 'records.'); 
         resolve(results.data);
       }
     });
   });
 };
-
-const test = async () => {
-  let parsedData = await readCSV(csvFilePath); 
-  console.log("Heres the data", parsedData[2])
+//Exports data to a csv file
+const writeCSV = async (data) => {
+ var csv = Papa.unparse(data);
+ fs.writeFileSync("Exported.csv", csv);
 }
 
-test()
+const runScript = async () => {
+  let parsedData = await readCSV(csvFilePath); 
+
+//Fills in the categories and subcategories for all entries. Runtime is O(2N)
+  let findObj = {};
+ for(let i = 0; i < parsedData.length; i++) {
+  let data = parsedData[i];
+  if (data['Category']) {
+   findObj[uniformString(data['Vendor'])] = {Category: data['Category'], Subcategory: data['Subcategory']};
+  }
+ }
+ for (let i = 0; i < parsedData.length; i++) {
+  let data = parsedData[i];
+  if (!data['Category']) {
+   let searchParam = uniformString(data['Vendor']);
+   data['Category'] = findObj[searchParam].Category;
+   data['Subcategory'] = findObj[searchParam].Subcategory;
+  }
+ }
+
+ //Sort the data alphabetically. Probably runtime of O(N * logN)
+ parsedData.sort(compare);
+
+ //Exporting to a CSV file
+writeCSV(parsedData);
+
+//Exports a report
+}
+
+runScript()
+
+
